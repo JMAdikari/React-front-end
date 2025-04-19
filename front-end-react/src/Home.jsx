cabba61d6de4af82408fb0c19e2d02963c2148a3
Jayani adikari <jayaniadikari19@gmail.com>
2025-04-20 02:23:55 +0530

    Added background image

--- front-end-react/src/Home.jsx
+++ front-end-react/src/Home.jsx
@@ -3,12 +3,14 @@ import './Home.css';
 
 const Home = ({ onNavigate }) => {
   return (
-    <div className="home-container">
-      <h1>Welcome to Our App</h1>
-      <p>Please choose an option to continue:</p>
-      <div className="home-buttons">
-        <button onClick={() => onNavigate('login')}>Login</button>
-        <button onClick={() => onNavigate('register')}>Register</button>
+    <div className="home-background">
+      <div className="home-content">
+        <h1>JWT Security Research Portal</h1>
+        <p>Welcome to the research interface.</p>
+        <div className="home-buttons">
+          <button onClick={() => onNavigate('login')}>Login</button>
+          <button onClick={() => onNavigate('register')}>Register</button>
+        </div>
       </div>
     </div>
   );
